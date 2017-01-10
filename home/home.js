11398e0bd71e0d8b2b74aec1fa1d75a2ae83a60c
Jing Ran <jingran.mavis@gmail.com>
2017-01-10 15:51:37 -0500

    Update animation duration

--- home/home.js
+++ home/home.js
@@ -1,19 +1,27 @@
-var readmore = document.querySelectorAll(".jt-article-preview__more"),
-  readMoreArray = [].slice.call(readmore);
+var readmore = document.querySelectorAll(".jt-article-preview__more");
+var readMoreArray = [].slice.call(readmore);
 
-var compress = document.querySelectorAll(".jt-article-preview__btn-compress"),
-  compressArray = [].slice.call(compress);
+var gotoBtn = document.querySelectorAll(".jt-article-preview__goto");
 
-var content = document.querySelectorAll(".jt-article-preview__content"),
-  contentWrap = document.querySelectorAll(".jt-article-preview__content-wrap"),
-  blurbContainer = document.querySelectorAll('.jt-article-preview'),
-  bg = document.querySelectorAll('.jt-article-preview__bg-wrap'),
-  pagination = document.querySelector('.jt-pagination');
+var compress = document.querySelectorAll(".jt-article-preview__btn-compress");
+var compressArray = [].slice.call(compress);
 
-var header = document.querySelector('header'),
-  footer = document.querySelector('footer');
+var close = document.querySelectorAll(".jt-article-preview__btn-close");
+var closeArray = [].slice.call(close);
 
-readmore.forEach(addExpandClickHandler);
+var content = document.querySelectorAll(".jt-article-preview__content");
+var contentWrap = document.querySelectorAll(".jt-article-preview__content-wrap");
+var blurbContainer = document.querySelectorAll('.jt-article-preview');
+var bg = document.querySelectorAll('.jt-article-preview__bg-wrap');
+var pagination = document.querySelector('.jt-pagination');
+
+var header = document.querySelector('header');
+var footer = document.querySelector('footer');
+var search = document.querySelector('.search-wrap');
+
+for (var i = 0; i < readmore.length; i++) {
+  addExpandClickHandler(readmore[i]);
+}
 
 function addExpandClickHandler(button) {
   //Get the index of the current button
@@ -24,7 +32,9 @@ function addExpandClickHandler(button) {
   });
 }
 
-compress.forEach(addCompressClickHandler);
+for (var j = 0; j < compress.length; j++) {
+  addCompressClickHandler(compress[j]);
+}
 
 function addCompressClickHandler(button) {
   //Get the index of the current button
@@ -35,10 +45,24 @@ function addCompressClickHandler(button) {
   })
 }
 
+for (var k = 0; k < close.length; k++) {
+  addCloseClickHandler(close[k]);
+}
+
+function addCloseClickHandler(button) {
+  //Get the index of the current button
+  var index = closeArray.indexOf(button);
+
+  button.addEventListener('click', function() {
+    closeArticle(index);
+  })
+}
+
 
 function hideRest(index) {
   header.style.visibility = "hidden";
-  footer.style.display = "none";
+  footer.style.opacity = 0;
+  search.style.display = "none";
   pagination.style.display = "none";
 
   for (var i = 0; i < readMoreArray.length; i++) {
@@ -50,7 +74,8 @@ function hideRest(index) {
 
 function displayAll(index) {
   header.style.visibility = "visible";
-  footer.style.display = "block";
+  footer.style.opacity = 1;
+  search.style.display = "block";
   pagination.style.display = "block";
 
   for (var i = 0; i < readMoreArray.length; i++) {
@@ -60,13 +85,16 @@ function displayAll(index) {
   }
 }
 
+var tl = new TimelineMax();
+
 function expandArticle(index) {
   //Hide all other elements
   hideRest(index);
 
   //Smooth scroll to top
-  TweenLite.to(window, 0.5, {
-    scrollTo: 0
+  TweenLite.to(window, 0, {
+    scrollTo: 0,
+    // delay: 0.1
   });
 
   //Scale background image
@@ -75,10 +103,10 @@ function expandArticle(index) {
     scale: 1
   });
 
-  TweenMax.to(bg[index], 0.5, {
-    scale: 2,
+  TweenMax.to(bg[index], 1, {
+    scale: 1.26,
     bottom: '35vh',
-    ease: Power3.easeOut
+    ease: Expo.easeOut
   });
 
   //Bring selected article to front
@@ -89,32 +117,48 @@ function expandArticle(index) {
   });
 
   //Bring content up a bit
-  TweenMax.to(contentWrap[index], 0.5, {
-    top: -100,
-    ease: Power3.easeOut
+  TweenMax.to(contentWrap[index], 1, {
+    top: -350,
+    ease: Expo.easeOut
   });
 
   //Expand content
   content[index].style.height = "auto";
 
-  TweenMax.from(content[index], 0.3, {
+  TweenMax.from(content[index], 1.7, {
     height: 350,
-    ease: Power3.easeIn
+    ease: Expo.easeIn,
+    delay: -0.5
   });
 
   //Hide readmore button
-  TweenMax.to(readmore[index], 0.5, {
+  TweenMax.to(readmore[index], 0.2, {
     css: {
       opacity: 0
     }
   });
   setTimeout(function() {
-  	readmore[index].style.visibility = 'hidden';
+    readmore[index].style.visibility = 'hidden';
   }, 500);
 
+  TweenMax.to(gotoBtn[index], 0.2, {
+    css: {
+      opacity: 0
+    }
+  });
+  setTimeout(function() {
+    gotoBtn[index].style.visibility = 'hidden';
+  }, 500);
+
+  //Reveal compress article button
+  // compress[index].style.display = 'block';
+  // TweenMax.to(compress[index], 0.5, {
+  //   css: {
+  //     opacity: 1
+  //   }
+  // });
   //Reveal close article button
-  compress[index].style.display = 'block';
-  TweenMax.to(compress[index], 0.5, {
+  TweenMax.to(close[index], 0.5, {
     css: {
       opacity: 1
     }
@@ -129,10 +173,10 @@ function closeArticle(index) {
   window.scrollTo(0, blurbContainer[index].offsetTop);
 
   //Scale background image
-  TweenMax.to(bg[index], 0.5, {
+  TweenMax.to(bg[index], 0.8, {
     scale: 1,
     bottom: 0,
-    ease: Power3.easeOut
+    ease: Expo.easeOut
   });
 
   //Reset z-index
@@ -143,15 +187,15 @@ function closeArticle(index) {
   });
 
   //Bring content down a bit
-  TweenMax.to(contentWrap[index], 0.5, {
+  TweenMax.to(contentWrap[index], 0.8, {
     top: 0,
-    ease: Power3.easeOut
+    ease: Expo.easeOut
   });
 
   //Compress content
   TweenMax.to(content[index], 0.5, {
     height: 350,
-    ease: Power3.easeOut
+    ease: Expo.easeOut
   });
 
   //Show readmore button
@@ -162,6 +206,19 @@ function closeArticle(index) {
   });
   readmore[index].style.visibility = 'visible';
 
-  //Reveal close article button
+  TweenMax.to(gotoBtn[index], 0.5, {
+    css: {
+      opacity: 1
+    }
+  });
+  gotoBtn[index].style.visibility = 'visible';
+
+  //Hide compress article button
   compress[index].style.display = 'none';
+  //Hide close article button
+  TweenMax.to(close[index], 0.5, {
+    css: {
+      opacity: 0
+    }
+  });
 }
